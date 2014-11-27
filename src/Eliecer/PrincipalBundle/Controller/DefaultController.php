9cf46b0cbee366c5007958a086b4a917d8ac3f57
Eliecer David Pari Alhuay <ele.developer.92@gmail.com>
2014-11-27 09:15:26 -0500

    PrincipalBundle
    Creacion del bundle PrincipalBundle

---
+++ src/Eliecer/PrincipalBundle/Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Eliecer\PrincipalBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction($name)
+    {
+        return $this->render('EliecerPrincipalBundle:Default:index.html.twig', array('name' => $name));
+    }
+}
---
+++ src/Eliecer/PrincipalBundle/EliecerPrincipalBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Eliecer\PrincipalBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class EliecerPrincipalBundle extends Bundle
+{
+}
